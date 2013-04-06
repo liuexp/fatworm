a1aee0e331d4d8000a52a6a3ff9eb5d9d8d72c5f
Jingcheng_Liu <liuexp@gmail.com>
2013-04-06 10:10:54 +0800

    meow

---
+++ src/fatworm/driver/Driver.java
@@ -0,0 +1,60 @@
+package fatworm.driver;
+
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+
+public class Driver implements java.sql.Driver {
+
+	public Driver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
