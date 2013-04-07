9ec5015795ff47f489712c9a9a8e4c9c7cac5100
liuexp <liuexp@8bf4c01e-07df-42e3-9aae-5d9143e42f0d>
2013-04-07 09:34:43 +0000

    meow
    git-svn-id: https://202.120.61.16/svn/liuexp/fatworm2013/trunk@3 8bf4c01e-07df-42e3-9aae-5d9143e42f0d

--- src/fatworm/driver/Driver.java
+++ src/fatworm/driver/Driver.java
@@ -16,44 +16,37 @@ public class Driver implements java.sql.Driver {
 
 	@Override
 	public Connection connect(String url, Properties info) throws SQLException {
-
 		return null;
 	}
 
 	@Override
 	public boolean acceptsURL(String url) throws SQLException {
-
 		return false;
 	}
 
 	@Override
 	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
 			throws SQLException {
-
 		return null;
 	}
 
 	@Override
 	public int getMajorVersion() {
-
 		return 0;
 	}
 
 	@Override
 	public int getMinorVersion() {
-
 		return 0;
 	}
 
 	@Override
 	public boolean jdbcCompliant() {
-
 		return false;
 	}
 
 	@Override
 	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-
 		return null;
 	}
 
