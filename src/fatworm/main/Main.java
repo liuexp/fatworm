911a3f8f3f47b36df2bf1de9fe7c376b431ff675
liuexp <liuexp@8bf4c01e-07df-42e3-9aae-5d9143e42f0d>
2013-04-15 13:31:56 +0000

    started group by scan design.
    git-svn-id: https://202.120.61.16/svn/liuexp/fatworm2013/trunk@33 8bf4c01e-07df-42e3-9aae-5d9143e42f0d

--- src/fatworm/main/Main.java
+++ src/fatworm/main/Main.java
@@ -18,14 +18,18 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		DBEngine db = new DBEngine();
 		Scanner in = new Scanner(System.in);
+		System.out.print(">");
 		while (in.hasNextLine()) {
 				try {
 					String tmp = in.nextLine();
+					@SuppressWarnings("unused")
 					ResultSet result = db.execute(tmp);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				System.out.print("\r\n>");
 		}
+		in.close();
 	}
 
 }
