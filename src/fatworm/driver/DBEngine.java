a68236cd61f5cf65618f90560e55715e1d1d3afd
Jingcheng Liu <liuexp@gmail.com>
2013-04-17 23:48:48 +0800

    DML framework

--- src/fatworm/driver/DBEngine.java
+++ src/fatworm/driver/DBEngine.java
@@ -25,12 +25,10 @@ import static fatworm.parser.FatwormParser.SELECT;
 import static fatworm.parser.FatwormParser.SELECT_DISTINCT;
 import static fatworm.parser.FatwormParser.UPDATE;
 import static fatworm.parser.FatwormParser.USE_DATABASE;
-import fatworm.absyn.*;
 import fatworm.logicplan.Plan;
 import fatworm.parser.FatwormLexer;
 import fatworm.parser.FatwormParser;
 import fatworm.util.Env;
-import fatworm.util.Traverse;
 import fatworm.util.Util;
 
 // It's true that byte code is much easier for (low-level) optimization, e.g. instruction re-order/elimination
@@ -39,6 +37,14 @@ public class DBEngine {
 
 	public String name;
 	public Map<String, Table> dbList;
+	private static DBEngine instance;
+	private Database db;
+
+	public static synchronized DBEngine getInstance() {
+		if (instance == null)
+			instance = new DBEngine();
+		return instance;
+	}
 	public DBEngine() {
 		// TODO Auto-generated constructor stub
 	}
@@ -49,7 +55,7 @@ public class DBEngine {
 		return execute(t);
 	}
 
-	private static ResultSet execute(CommonTree t) throws SQLException {
+	private ResultSet execute(CommonTree t) throws SQLException {
 		switch(t.getType()){
 		case SELECT:
 		case SELECT_DISTINCT:
@@ -61,6 +67,20 @@ public class DBEngine {
 			else 
 				System.out.println("no results");
 			return new ResultSet(x);
+		case CREATE_DATABASE:
+		case USE_DATABASE:
+		case DROP_DATABASE:
+		case CREATE_TABLE:
+		case DROP_TABLE:
+		case INSERT_VALUES:
+		case INSERT_COLUMNS:
+		case INSERT_SUBQUERY:
+		case DELETE:
+		case UPDATE:
+		case CREATE_INDEX:
+		case CREATE_UNIQUE_INDEX:
+		case DROP_INDEX:
+			//TODO
 		default:
 				throw new RuntimeException("not implemented.");
 		}
@@ -73,6 +93,10 @@ public class DBEngine {
 		FatwormParser parser = new FatwormParser(tokens);
 		FatwormParser.statement_return r = parser.statement();
 		return (CommonTree) r.getTree();
+	}
+	public void close() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/fatworm/driver/Table.java
+++ src/fatworm/driver/Table.java
@@ -1,12 +1,15 @@
 package fatworm.driver;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.antlr.runtime.tree.CommonTree;
 
 import fatworm.absyn.Expr;
+import fatworm.util.Env;
 
+// FIXME temporary table in memory.
 public class Table {
 	public Schema schema;
 	public List<Record> records = new ArrayList<Record>();
@@ -14,7 +17,32 @@ public class Table {
 		schema = new Schema(t);
 	}
 	
-	public void delete(Expr e){
-		for
+	public int delete(Expr e){
+		int ret = 0;
+		for(Iterator<Record> itr = records.iterator();itr.hasNext();){
+			Record r = itr.next();
+			Env env = new Env();
+			env.appendFromRecord(r);
+			if(!e.evalPred(env))continue;
+			itr.remove();
+			ret++;
+			
+		}
+		return ret;
+	}
+	
+	public int update(List<String> colName, List<Expr> expr, Expr e){
+		int ret = 0;
+		for(Record r:records){
+			Env env = new Env();
+			env.appendFromRecord(r);
+			if(e!=null && !e.evalPred(env))continue;
+			for(int i=0;i<expr.size();i++){
+				Env localEnv = env.clone();
+				r.cols.set(r.schema.findIndex(colName.get(i)), expr.get(i).eval(localEnv));
+			}
+			ret++;
+		}
+		return ret;
 	}
 }
