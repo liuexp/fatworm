360ef0d4f564c315a935084fd4c0a5d42539dca9
liuexp <liuexp@8bf4c01e-07df-42e3-9aae-5d9143e42f0d>
2013-06-01 05:48:32 +0000

    meow
    git-svn-id: https://202.120.61.16/svn/liuexp/fatworm2013/trunk@101 8bf4c01e-07df-42e3-9aae-5d9143e42f0d

---
+++ src/fatworm/io/IndexCursor.java
@@ -0,0 +1,52 @@
+package fatworm.io;
+
+public class IndexCursor implements Cursor {
+
+	public boolean reverse;// is reverse iterator
+	public IndexCursor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prev() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object fetch(String col) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete() throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
