6a39ea5fa5a0c43f877bcfa3941305483d53666c
liuexp <liuexp@8bf4c01e-07df-42e3-9aae-5d9143e42f0d>
2013-06-01 05:48:59 +0000

    meow
    git-svn-id: https://202.120.61.16/svn/liuexp/fatworm2013/trunk@117 8bf4c01e-07df-42e3-9aae-5d9143e42f0d

--- src/fatworm/logicplan/FetchTable.java
+++ src/fatworm/logicplan/FetchTable.java
@@ -1,24 +1,30 @@
 package fatworm.logicplan;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import fatworm.absyn.BinaryExpr;
+import fatworm.absyn.BinaryOp;
+import fatworm.absyn.Expr;
+import fatworm.absyn.Id;
 import fatworm.driver.DBEngine;
 import fatworm.driver.Record;
 import fatworm.driver.Schema;
 import fatworm.driver.Table;
+import fatworm.field.Field;
 import fatworm.util.Env;
 import fatworm.util.Util;
 import fatworm.io.Cursor;
 
 public class FetchTable extends Plan {
 
-	// TODO use index to accelerate
 	public String tableName;
 	public Table table;
 	Schema schema;
 	Cursor cursor;
-	// FIXME why don't we just use table.schema, even for database on disk
 	public FetchTable(String table) {
 		super();
 		tableName = table;
@@ -33,10 +39,56 @@ public class FetchTable extends Plan {
 		schema.primaryKey = this.table.getSchema().primaryKey;
 	}
 
-	// TODO Range Fetch?
 	@Override
 	public void eval(Env env) {
 		hasEval = true;
+		if(parent instanceof Select){
+			List<Cond> condList = new ArrayList<Cond>();
+			Map<String, List<Cond>> condGroup = new HashMap<String, List<Cond>>();
+			Map<String, Interval> condInt = new HashMap<String, Interval>();
+			Select cur = (Select) parent;
+			// first collect the conditions
+			while(true){
+				if(cur.pred instanceof BinaryExpr){
+					BinaryExpr cond = (BinaryExpr)cur.pred;
+					if(cond.op != BinaryOp.NEQ &&(cond.l instanceof Id && cond.r.isConst)||(cond.r instanceof Id && cond.l.isConst)){
+						Cond tmp = new Cond((BinaryExpr)cur.pred);
+						condList.add(tmp);
+						if(!condGroup.containsKey(tmp.name))
+							condGroup.put(tmp.name, new ArrayList<Cond>());
+						condGroup.get(tmp.name).add(tmp);
+					}
+				}
+				if(cur.parent instanceof Select)
+					cur = (Select)cur.parent;
+				else 
+					break;
+			}
+			// check for non-trivial interval specification.
+			boolean nonEmpty = true;
+			for(Map.Entry<String, List<Cond>> e : condGroup.entrySet()){
+				List<Cond> conds = e.getValue();
+				Interval x = new Interval();
+				for(Cond c:conds){
+					x.intersect(c.getInteval());
+					if(x.isEmpty()){
+						nonEmpty = false;
+						break;
+					}
+				}
+				if(!nonEmpty)
+					break;
+				condInt.put(e.getKey(), x);
+			}
+			if(!nonEmpty){
+				cursor = new EmptyCursor();
+				return;
+			}
+			
+			List<String> condName = new LinkedList<String>(condInt.keySet());
+			for(String name:condName){
+			}
+		}
 		cursor = table.open();
 	}
 	@Override
@@ -65,10 +117,9 @@ public class FetchTable extends Plan {
 		try {
 			cursor.reset();
 		} catch (Throwable e) {
-//			e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-
 	@Override
 	public Schema getSchema() {
 		return schema;
@@ -88,5 +139,114 @@ public class FetchTable extends Plan {
 	@Override
 	public List<String> getRequestedColumns() {
 		return new LinkedList<String>();
+	}
+	
+	private class Cond{
+		public BinaryOp op;
+		public String name;
+		public Field value;
+		public Cond(BinaryExpr e){
+			op = e.op;
+			if(e.l instanceof Id){
+				name = Util.getAttr(((Id)e.l).name);
+				value = e.r.eval(new Env());
+			}else{
+				name = Util.getAttr(((Id)e.r).name);
+				value = e.l.eval(new Env());
+			}
+		}
+		public Interval getInteval() {
+			switch(op){
+			case EQ:
+				return new Interval(value, value);
+			case LESS:
+			case LESS_EQ:
+				return new Interval(null, value);
+			case GREATER:
+			case GREATER_EQ:
+				return new Interval(value, null);
+			}
+			Util.error("Missing interval generation for operator:" + op);
+			return null;
+		}
+	}
+	
+	private class Interval{
+		public Field min;
+		public Field max;
+		public Interval(){
+			min = null;
+			max = null;
+		}
+		public Interval(Field l, Field r){
+			min = l;
+			max = r;
+		}
+		public void intersect(Interval x){
+			if(isNull(min) || (!isNull(x.min) && min.applyWithComp(BinaryOp.LESS, x.min)))
+				min = x.min;
+			if(isNull(max) || (!isNull(x.max) && max.applyWithComp(BinaryOp.GREATER, x.max)))
+				max = x.max;
+		}
+		private boolean isNull(Field x){
+			return x==null||x.type==java.sql.Types.NULL;
+		}
+		public boolean isEmpty(){
+			return !isNull(min) && !isNull(max) && min.applyWithComp(BinaryOp.GREATER, max);
+		}
+	}
+	
+	private class EmptyCursor implements Cursor{
+
+		@Override
+		public void reset() throws Throwable {
+		}
+
+		@Override
+		public void next() throws Throwable {
+		}
+
+		@Override
+		public void prev() throws Throwable {
+		}
+
+		@Override
+		public Object fetch(String col) {
+			return null;
+		}
+
+		@Override
+		public Object[] fetch() {
+			return null;
+		}
+
+		@Override
+		public void delete() throws Throwable {
+		}
+
+		@Override
+		public void close() {
+		}
+
+		@Override
+		public boolean hasNext() throws Throwable {
+			return false;
+		}
+
+		@Override
+		public Record fetchRecord() {
+			return null;
+		}
+
+		@Override
+		public Integer getIdx() {
+			return null;
+		}
+
+		@Override
+		public boolean hasThis() {
+			return false;
+		}
+		
 	}
 }
